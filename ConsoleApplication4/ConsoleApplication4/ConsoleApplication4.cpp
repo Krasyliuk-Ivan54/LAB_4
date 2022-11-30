eb899226b1d60dd7d4be7811456e609a143fcd8a
Krasyliuk_Ivan <115323833+Krasyliuk-Ivan54@users.noreply.github.com>
2022-11-30 16:15:25 +0200

    Update ConsoleApplication4.cpp

--- ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -5,25 +5,22 @@ int main()
 {
     int a[100];
     int i, n, dobutok;
-
-
-    //Кількість елементів масиву що будуть ініцілізуватись:
+    
     cout << "Vvedit kilkist elementiv\n ";
     cin >> n;
-
-    //Ввведення елементів
+    
     cout << "Vvedennya elementiv\n";
     for (i = 0; i < n; i++)
         cin >> a[i];
 
     for (i = 0; i < n; i++)
-        cout << "a[" << i << "]=" << a[i] << endl; //виведення елементів масиву
+        cout << "a[" << i << "]=" << a[i] << endl; 
 
     dobutok = 1;
     for (i = 0; i < n; i++)
     {
         if (a[i] > 0)
-            dobutok = dobutok * a[i];    //множимо елементи масиву
+            dobutok = dobutok * a[i];    
     }
 
     cout << "dobutok= " << dobutok << endl;
